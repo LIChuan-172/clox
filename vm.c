78f6fabffbfe670fbf70a155f152c1704674b1e8
LI Chuan <lichuan172@gmail.com>
2024-07-10 22:42:59 +0800

    feat: finish chapter 20

--- vm.c
+++ vm.c
@@ -36,11 +36,14 @@ void initVM()
 {
   resetStack();
   vm.objects = NULL;
+
+  initTable(&vm.globals);
   initTable(&vm.strings);
 }
 
 void freeVM()
 {
+  freeTable(&vm.globals);
   freeTable(&vm.strings);
   freeObjects();
 }
@@ -86,6 +89,7 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define READ_STRING_CONSTANT() AS_STRING(READ_CONSTANT())
 #define BINARY_OP(valueType, op)                    \
   do                                                \
   {                                                 \
@@ -130,6 +134,39 @@ static InterpretResult run()
     case OP_FALSE:
       push(BOOL_VAL(false));
       break;
+    case OP_POP:
+      pop();
+      break;
+    case OP_GET_GLOBAL:
+    {
+      ObjString *name = READ_STRING_CONSTANT();
+      Value value;
+      if(!tableGet(&vm.globals, name, &value))
+      {
+        runtimeError("Undefined variable '%s'.", name->chars);
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      push(value);
+      break;
+    }
+    case OP_DEFINE_GLOBAL:
+    {
+      ObjString *name = READ_STRING_CONSTANT();
+      tableSet(&vm.globals, name, peek(0));
+      pop();
+      break;
+    }
+    case OP_SET_GLOBAL: {
+      ObjString *name = READ_STRING_CONSTANT();
+      if(tableSet(&vm.globals, name, peek(0)))
+      {
+        tableDelete(&vm.globals, name);
+        runtimeError("Undefined variable '%s'.", name->chars);
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      // Don't pop the value, because assignment expressions return the value
+      break;
+    }
     case OP_EQUAL:
       Value b = pop();
       Value a = pop();
@@ -190,10 +227,15 @@ static InterpretResult run()
       push(NUMBER_VAL(-AS_NUMBER(pop())));
       break;
     }
-    case OP_RETURN:
+    case OP_PRINT:
     {
       printValue(pop());
       printf("\n");
+      break;
+    }
+    case OP_RETURN:
+    {
+      // Exit interpreter
       return INTERPRET_OK;
     }
     }
@@ -201,6 +243,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef READ_STRING
 #undef BINARY_OP
 }
 
